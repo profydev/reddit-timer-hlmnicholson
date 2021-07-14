a75f43dd095938e5f27266394f2db50b8cdffeb4
hlmnicholson <nicholson.helena@gmail.com>
2021-07-14 11:08:26 +0200

    created app skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Search from './Components/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
