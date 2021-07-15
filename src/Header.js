28b9be34bd0c6306f64bbf2de07b2be94fe2da37
Helena Nicholson <Nicholson.helena@gmail.com>
2021-07-15 16:00:29 +0200

    logo in Header is an SVG and imported into component

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,38 @@
 import React from 'react';
+// import styled from 'styled-components';
+// import { ReactComponent as Logo } from './logo.svg';
+
+// // const Logo = styled.img`
+// //   width: 500px;
+// //   height: auto;
+// //   padding: 20px;
+// // `;
+
+// // function LogoImg() {
+// //   return <div><img src={image} alt="reddit timer logo and link to homepage" /></div>
+// // }
+
+// function Header() {
+//   return (
+//     <header>
+//       {/* <LogoImage> */}
+//       <Logo />
+//       {/* </LogoImage> */}
+//       Header PlaceHolder
+//     </header>
+
+//   );
+// }
+import logo from './logo.svg'; // Tell webpack this JS file uses this image
+
+const Logo = () => <img src={logo} alt="Logo" />;
 
 function Header() {
   return (
     <header>
+      {/* <LogoImage> */}
+      <Logo />
+      {/* </LogoImage> */}
       Header PlaceHolder
     </header>
 
