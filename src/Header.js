8e2231c629c8dfb3c1cf4a191a184088d22bc4b5
Helena Nicholson <Nicholson.helena@gmail.com>
2021-07-15 16:11:58 +0200

    adjust logo to be working <Link> to home route (/)

--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,13 @@
 import React from 'react';
-// import styled from 'styled-components';
-// import { ReactComponent as Logo } from './logo.svg';
+import { Link } from 'react-router-dom';
+import logo from './logo.svg';
 
-// // const Logo = styled.img`
-// //   width: 500px;
-// //   height: auto;
-// //   padding: 20px;
-// // `;
-
-// // function LogoImg() {
-// //   return <div><img src={image} alt="reddit timer logo and link to homepage" /></div>
-// // }
-
-// function Header() {
-//   return (
-//     <header>
-//       {/* <LogoImage> */}
-//       <Logo />
-//       {/* </LogoImage> */}
-//       Header PlaceHolder
-//     </header>
-
-//   );
-// }
-import logo from './logo.svg'; // Tell webpack this JS file uses this image
-
-const Logo = () => <img src={logo} alt="Logo" />;
+const Logo = () => <Link to="/"><img src={logo} alt="Logo" /></Link>;
 
 function Header() {
   return (
     <header>
-      {/* <LogoImage> */}
       <Logo />
-      {/* </LogoImage> */}
       Header PlaceHolder
     </header>
 
