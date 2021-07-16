c2374711b8c7d9b356330a30fa9d6823ce58ef70
Helena Nicholson <Nicholson.helena@gmail.com>
2021-07-16 14:33:04 +0200

    Search link points to search page with javascript as default query

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Logo from './Logo';
+import Search from './Search';
 
 function Header() {
   return (
     <header>
       <Logo />
-      <a href="#how-it-works">How it works</a>
-      <a href="#about">About</a>
-      <Link to="/search?q=javascript">Search</Link>
+      <Search />
+      <a href="/#how-it-works">How it works</a>
+      <a href="/#about">About</a>
     </header>
-
   );
 }
 
---
+++ src/Search.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Search = () => <Link to="/search?q=javascript">Search</Link>;
+
+export default Search;
