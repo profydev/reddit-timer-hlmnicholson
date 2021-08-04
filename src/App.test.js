f1119d77faa7ad048bce494acf8fd9ed28671034
Helena Nicholson <Nicholson.helena@gmail.com>
2021-08-04 11:29:22 +0200

    wrote first test on header (check whether logo link is pointing to '/') using jest and react testing library

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import fetchMock from 'jest-fetch-mock';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
-// import mockResponse from './__mocks__/subreddit-reactjs-response.json';
-
-fetchMock.enableMocks();
 
 function setup() {
   return render(
@@ -23,24 +19,8 @@ describe('Header', () => {
     const link = screen.getByRole('link', { name: /logo/i });
     userEvent.click(link);
 
-    expect(screen.getByText(/Home/i)).toBeInTheDocument();
+    expect(screen.getByText(/home/i)).toBeInTheDocument();
 
     screen.debug();
   });
-
-  // test.each([
-  //   [/about/i],
-  //   // [/how it works/i],
-  // ])('link points to the correct page', (label) => {
-  //   setup();
-
-  //   const link = screen.getByRole('link', { name: label });
-  //   userEvent.click(link);
-
-  //   expect(
-  //     screen.getByRole('heading', { name: label }),
-  //   ).toBeInTheDocument();
-
-  //   screen.debug();
-  // });
 });
